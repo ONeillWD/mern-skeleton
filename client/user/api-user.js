88bed2fc15573f2b07ee7ffeae7d862772ba7bc2
Aidan O'Neill <75544718+ONeillWD@users.noreply.github.com>
2021-06-15 19:47:30 -0400

    initial code additions

--- client/user/api-user.js
+++ client/user/api-user.js
@@ -1,13 +1,13 @@
 const create = async (user) => {
   try {
-      let response = await fetch('/api/users/', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(user)
-      })
+    let response = await fetch('/api/users/', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
     return await response.json()
   } catch(err) {
     console.log(err)
@@ -18,7 +18,7 @@ const list = async (signal) => {
   try {
     let response = await fetch('/api/users/', {
       method: 'GET',
-      signal: signal,
+      signal: signal
     })
     return await response.json()
   } catch(err) {
@@ -30,13 +30,12 @@ const read = async (params, credentials, signal) => {
   try {
     let response = await fetch('/api/users/' + params.userId, {
       method: 'GET',
-      signal: signal,
       headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + credentials.t
       }
-    })
+  })
     return await response.json()
   } catch(err) {
     console.log(err)
@@ -49,10 +48,9 @@ const update = async (params, credentials, user) => {
       method: 'PUT',
       headers: {
         'Accept': 'application/json',
-        'Content-Type': 'application/json',
         'Authorization': 'Bearer ' + credentials.t
       },
-      body: JSON.stringify(user)
+      body: user
     })
     return await response.json()
   } catch(err) {
@@ -76,10 +74,64 @@ const remove = async (params, credentials) => {
   }
 }
 
+const follow = async (params, credentials, followId) => {
+  try {
+    let response = await fetch('/api/users/follow/', {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      },
+      body: JSON.stringify({userId:params.userId, followId: followId})
+    })
+    return await response.json()
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+const unfollow = async (params, credentials, unfollowId) => {
+  try {
+    let response = await fetch('/api/users/unfollow/', {
+      method: 'PUT',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      },
+      body: JSON.stringify({userId:params.userId, unfollowId: unfollowId})
+    })
+    return await response.json()
+  } catch(err) {
+    console.log(err)
+  }
+}
+
+const findPeople = async (params, credentials, signal) => {
+  try {
+    let response = await fetch('/api/users/findpeople/' + params.userId, {
+      method: 'GET',
+      signal: signal,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + credentials.t
+      }
+    })    
+    return await response.json()
+  } catch(err) {
+    console.log(err)
+  }
+}
+
 export {
   create,
   list,
   read,
   update,
-  remove
+  remove,
+  follow,
+  unfollow,
+  findPeople
 }
